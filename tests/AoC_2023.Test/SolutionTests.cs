8c8bf02aac7ea696e2d13034430b2f12b4d5f1e7
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2023-11-26 00:12:24 +0100

    Make CI happy :D

--- tests/AoC_2023.Test/SolutionTests.cs
+++ tests/AoC_2023.Test/SolutionTests.cs
@@ -6,7 +6,7 @@ namespace AoC_2023.Test;
 #pragma warning disable IL2067 // Target parameter argument does not satisfy 'DynamicallyAccessedMembersAttribute' in call to target method. The parameter of method does not have matching annotations.
 public static class SolutionTests
 {
-    [TestCase(typeof(Day_01), "0", "0")]
+    [TestCase(typeof(Day_01), "Solution to Day 1, part 1", "Solution to Day 1, part 2")]
     public static async Task Test(Type type, string sol1, string sol2)
     {
         if (Activator.CreateInstance(type) is BaseProblem instance)
