73624fd2f9f2fd0dd5da773d1c0ac388cf910865
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2023-12-03 15:52:52 +0100

    Add benchmark for day 3

---
+++ tests/AoC_2023.Benchmarks/Day_03_Benchmarks.cs
@@ -0,0 +1,20 @@
+﻿/*
+ *
+ *  | Method | Mean     | Error     | StdDev    | Gen0     | Gen1    | Allocated |
+ *  |------- |---------:|----------:|----------:|---------:|--------:|----------:|
+ *  | Day_03 | 6.921 ms | 0.1345 ms | 0.1929 ms | 171.8750 | 93.7500 | 807.39 KB |
+ *
+*/
+
+namespace AoC_2023.Benchmarks;
+
+public class Day_03_Full : BaseDayBenchmark
+{
+    [Benchmark]
+    public async Task Day_03()
+    {
+        var day3 = new Day_03();
+        await day3.Solve_1();
+        await day3.Solve_2();
+    }
+}
