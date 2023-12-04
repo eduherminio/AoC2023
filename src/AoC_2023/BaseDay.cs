2579054b0bd9939da7bfc4861ab1830368bb2527
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2023-12-04 22:57:30 +0100

    Always use output copied inputs, allowing more flexibility when solving problems from other assembly

---
+++ src/AoC_2023/BaseDay.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+
+namespace AoC_2023;
+
+public abstract class BaseDay : AoCHelper.BaseDay
+{
+    protected override string InputFileDirPath =>
+        Path.Combine(
+            Path.GetDirectoryName(Assembly.GetEntryAssembly()!.Location)!,
+            base.InputFileDirPath);
+}
