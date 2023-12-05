ae3159a30c32a3f8732d9b01f70a9509cb5a0003
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2023-12-05 12:35:51 +0100

    Add Day 5 part 1

---
+++ src/AoC_2023/Day_05.cs
@@ -0,0 +1,80 @@
+﻿namespace AoC_2023;
+
+internal sealed record Map(ulong DestinationRangeStart, ulong SourceRangeStart, ulong RangeLength)
+{
+    public ulong To(ulong seed)
+    {
+        if(seed >= SourceRangeStart)
+        {
+            var diff = seed - SourceRangeStart;
+
+            if(diff < RangeLength)
+            {
+                return DestinationRangeStart + diff;
+            }
+        }
+
+        return seed;
+    }
+}
+
+public class Day_05 : BaseDay
+{
+    private sealed record Input(List<ulong> InitialSeeds, List<List<Map>> FromToMaps);
+
+    private readonly Input _input;
+
+    public Day_05()
+    {
+        _input = ParseInput();
+    }
+
+    public override ValueTask<string> Solve_1() => new($"{Solve_1_Original()}");
+
+    public override ValueTask<string> Solve_2() => new($"{Solve_2_Original()}");
+
+    public ulong Solve_1_Original()
+    {
+        return _input.InitialSeeds.Min(seed =>
+            _input.FromToMaps.Aggregate(seed, (current, next) => next.GroupBy(m => m.To(current)).Last().Key));
+    }
+
+    public ulong Solve_2_Original()
+    {
+        return 0;
+    }
+
+    private Input ParseInput()
+    {
+        var groupOfLines = ParsedFile.ReadAllGroupsOfLines(InputFilePath);
+        var seedStrings = groupOfLines[0][0].Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+        List<ulong> seeds = new(seedStrings.Length - 1);
+        for (int i = 1; i < seedStrings.Length; ++i)
+        {
+            seeds.Add(ulong.Parse(seedStrings[i]));
+        }
+
+        var input = new Input(seeds, new(groupOfLines.Count - 1));
+
+        for (int i = 1; i < groupOfLines.Count; ++i)
+        {
+            input.FromToMaps.Add(new(groupOfLines[i].Count));
+
+            for (int j = 1; j < groupOfLines[i].Count; ++j)
+            {
+                var line = groupOfLines[i][j];
+
+                var mapStrings = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                if (mapStrings.Length != 3)
+                {
+                    throw new SolvingException();
+                }
+
+                input.FromToMaps.Last().Add(new(ulong.Parse(mapStrings[0]), ulong.Parse(mapStrings[1]), ulong.Parse(mapStrings[2])));
+            }
+        }
+
+        return input;
+    }
+}
---
+++ tests/AoC_2023.Test/Day_05Tests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace AoC_2023.Test;
+public class Day_05Tests
+{
+    [TestCase(50ul, 98ul, 2ul, 98ul, 50ul)]
+    [TestCase(50ul, 98ul, 2ul, 99ul, 51ul)]
+    [TestCase(50ul, 98ul, 2ul, 97ul, 97ul)]
+    [TestCase(50ul, 98ul, 2ul, 100ul, 100ul)]
+
+    [TestCase(52ul, 50ul, 48ul, 53ul, 55ul)]
+    [TestCase(52ul, 50ul, 48ul, 49ul, 49ul)]
+    [TestCase(52ul, 50ul, 48ul, 50ul, 52ul)]
+    [TestCase(52ul, 50ul, 48ul, 51ul, 53ul)]
+    [TestCase(52ul, 50ul, 48ul, 96ul, 98ul)]
+    [TestCase(52ul, 50ul, 48ul, 97ul, 99ul)]
+    public void Map_To(ulong destinationRangeStart, ulong sourceRangeStart, ulong rangeLength, ulong from, ulong expectedTo)
+    {
+        Map map = new(destinationRangeStart, sourceRangeStart, rangeLength);
+
+        ClassicAssert.AreEqual(expectedTo, map.To(from));
+    }
+}
