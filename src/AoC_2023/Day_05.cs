268d126e5cb245d53421583508b001e4610078a2
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2023-12-06 22:18:56 +0100

    Add day 5 part2: 3min 46s, but still counts

--- src/AoC_2023/Day_05.cs
+++ src/AoC_2023/Day_05.cs
@@ -1,8 +1,8 @@
 ﻿namespace AoC_2023;
 
-internal sealed record Map(ulong DestinationRangeStart, ulong SourceRangeStart, ulong RangeLength)
+internal sealed record Map(long DestinationRangeStart, long SourceRangeStart, long RangeLength)
 {
-    public ulong To(ulong seed)
+    public long To(long seed)
     {
         if (seed >= SourceRangeStart)
         {
@@ -20,7 +20,7 @@ internal sealed record Map(ulong DestinationRangeStart, ulong SourceRangeStart, 
 
 public class Day_05 : BaseDay
 {
-    private sealed record Input(List<ulong> InitialSeeds, List<List<Map>> FromToMaps);
+    private sealed record Input(List<long> InitialSeeds, List<List<Map>> FromToMaps);
 
     private readonly Input _input;
 
@@ -31,101 +31,115 @@ public class Day_05 : BaseDay
 
     public override ValueTask<string> Solve_1() => new($"{Solve_1_Original()}");
 
-    public override ValueTask<string> Solve_2() => throw new NotImplementedException();
+    public override ValueTask<string> Solve_2() => new($"{Solve_2_Original()}");
 
-    public ulong Solve_1_Original()
+    public long Solve_1_Original()
     {
         return _input.InitialSeeds.Min(seed =>
             _input.FromToMaps.Aggregate(seed, (current, next) => next.GroupBy(m => m.To(current)).OrderByDescending(g => g.Count()).Last().Key));
     }
 
-    public ulong Solve_2_Original_NoArray()
+    public long Solve_2_Original()
     {
-        List<ulong> seeds = new List<ulong>(_input.InitialSeeds.Count * 1000);
+        var min = long.MaxValue;
+        var max = 0L;
 
-        for (int i = 0; i < _input.InitialSeeds.Count - 1; i += 2)
+        foreach (var mapLayer in _input.FromToMaps)
         {
-            for (int j = 0; j < (int)_input.InitialSeeds[i + 1]; ++j)
+            foreach (var map in mapLayer)
             {
-                seeds.Add(_input.InitialSeeds[i] + (ulong)j);
+                if (map.SourceRangeStart < min)
+                {
+                    min = map.SourceRangeStart;
+                }
+
+                long end = map.SourceRangeStart + map.RangeLength;
+                if (end > max)
+                {
+                    max = end;
+                }
             }
         }
 
-        return seeds.Min(seed =>
-            _input.FromToMaps.Aggregate(seed, (current, next) => next.GroupBy(m => m.To(current)).OrderByDescending(g => g.Count()).Last().Key));
-    }
+        int maxArrayLength = (int)Math.Min(max - min, Array.MaxLength);
+        var arraysNeeded = 1 + ((int)((max - min) / maxArrayLength));
 
-    private static readonly object _lockObj = new object();
+        var currentArrays = new List<bool[]>(arraysNeeded);
+        for (int i = 0; i < arraysNeeded; i++)
+        {
+            currentArrays.Add(new bool[maxArrayLength]);
+        }
+        foreach (var initialSeed in InitialSeeds())
+        {
+            var arrayIndex = (int)Math.DivRem(initialSeed, maxArrayLength, out var itemIndex);
+            currentArrays[arrayIndex][itemIndex] = true;
+        }
 
-    public ulong Solve_2_NoArray_Foreach()
-    {
-        IEnumerable<ulong> InitialSeeds()
+        foreach (var mapLayer in _input.FromToMaps)
+        {
+            var nextArrays = new List<bool[]>(arraysNeeded);
+            for (int i = 0; i < arraysNeeded; i++)
+            {
+                nextArrays.Add(new bool[maxArrayLength]);
+            }
+
+            foreach (var map in mapLayer)
+            {
+                for (var i = map.SourceRangeStart; i < map.SourceRangeStart + map.RangeLength; ++i)
+                {
+                    var currentArrayIndex = (int)Math.DivRem(i, maxArrayLength, out var currentItemIndex);
+
+                    if (currentArrays[currentArrayIndex][currentItemIndex])
+                    {
+                        currentArrays[currentArrayIndex][currentItemIndex] = false;   // To be able to distinguish the unaffectd ones
+
+                        var valueToTransform = ((long)currentArrayIndex * maxArrayLength) + currentItemIndex;
+                        var newItemValue = map.To(valueToTransform);
+                        var nextArrayIndex = (int)Math.DivRem(newItemValue, maxArrayLength, out var nextItemIndex);
+
+                        nextArrays[nextArrayIndex][nextItemIndex] = true;
+                    }
+                }
+            }
+
+            // Letting through those ones unaffected by the mapping layer
+            for (int arrayIndex = 0; arrayIndex < currentArrays.Count; ++arrayIndex)
+            {
+                for (int itemIndex = 0; itemIndex < currentArrays[arrayIndex].Length; ++itemIndex)
+                {
+                    if (currentArrays[arrayIndex][itemIndex])
+                    {
+                        nextArrays[arrayIndex][itemIndex] = true;
+                    }
+                }
+            }
+
+            currentArrays = nextArrays;
+        }
+
+        for (int i = 0; i < currentArrays.Count; ++i)
+        {
+            for (int j = 0; j < currentArrays[i].Length; ++j)
+            {
+                if (currentArrays[i][j])
+                {
+                    return (i * (long)maxArrayLength) + j;
+                }
+            }
+        }
+
+        throw new SolvingException();
+
+        IEnumerable<long> InitialSeeds()
         {
             for (int i = 0; i < _input.InitialSeeds.Count - 1; i += 2)
             {
                 for (int j = 0; j < (int)_input.InitialSeeds[i + 1]; ++j)
                 {
-                    yield return _input.InitialSeeds[i] + (ulong)j;
+                    yield return _input.InitialSeeds[i] + j;
                 }
             }
         }
-
-        ulong index = 0;
-        var minLocation = ulong.MaxValue;
-        foreach (var seed in InitialSeeds())
-        {
-            if(index % 100_000 == 0)
-            {
-                Console.WriteLine(index);
-            }
-
-            var location = _input.FromToMaps.Aggregate(seed, (current, next) => next.GroupBy(m => m.To(current)).OrderByDescending(g => g.Count()).Last().Key);
-            if (location < minLocation)
-            {
-                minLocation = location;
-            }
-            ++index;
-        }
-
-        return minLocation;
-    }
-
-    public ulong Solve_2_NoArray_ParallelForeach()
-    {
-        IEnumerable<ulong> InitialSeeds()
-        {
-            for (int i = 0; i < _input.InitialSeeds.Count - 1; i += 2)
-            {
-                for (int j = 0; j < (int)_input.InitialSeeds[i + 1]; ++j)
-                {
-                    yield return _input.InitialSeeds[i] + (ulong)j;
-                }
-            }
-        }
-
-        var minLocation = ulong.MaxValue;
-        ulong index = 0;
-
-        Parallel.ForEach(InitialSeeds(), seed =>
-        {
-            if (index % 100_000 == 0)
-            {
-                Console.WriteLine(index);
-            }
-
-            var location = _input.FromToMaps.Aggregate(seed, (current, next) => next.GroupBy(m => m.To(current)).OrderByDescending(g => g.Count()).Last().Key);
-            lock (_lockObj)
-            {
-                if (location < minLocation)
-                {
-                    minLocation = location;
-                }
-            }
-
-            Interlocked.Increment(ref index);
-        });
-
-        return minLocation;
     }
 
     private Input ParseInput()
@@ -133,10 +147,10 @@ public class Day_05 : BaseDay
         var groupOfLines = ParsedFile.ReadAllGroupsOfLines(InputFilePath);
         var seedStrings = groupOfLines[0][0].Split(' ', StringSplitOptions.RemoveEmptyEntries);
 
-        List<ulong> seeds = new(seedStrings.Length - 1);
+        List<long> seeds = new(seedStrings.Length - 1);
         for (int i = 1; i < seedStrings.Length; ++i)
         {
-            seeds.Add(ulong.Parse(seedStrings[i]));
+            seeds.Add(long.Parse(seedStrings[i]));
         }
 
         var input = new Input(seeds, new(groupOfLines.Count - 1));
@@ -155,7 +169,7 @@ public class Day_05 : BaseDay
                     throw new SolvingException();
                 }
 
-                input.FromToMaps.Last().Add(new(ulong.Parse(mapStrings[0]), ulong.Parse(mapStrings[1]), ulong.Parse(mapStrings[2])));
+                input.FromToMaps.Last().Add(new(long.Parse(mapStrings[0]), long.Parse(mapStrings[1]), long.Parse(mapStrings[2])));
             }
         }
 
