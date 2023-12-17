1009665ecaead6d0237907f8daea26d82689a608
Eduardo Cáceres <eduardo.caceresdelacalle@gmail.com>
2023-12-17 16:54:49 +0100

    Refactor day 11

--- src/AoC_2023/Day_11.cs
+++ src/AoC_2023/Day_11.cs
@@ -18,48 +18,31 @@ public class Day_11 : BaseDay
 
     private readonly List<int> _xGaps;
     private readonly List<int> _yGaps;
+    private readonly List<Point> _galaxies;
 
     public Day_11()
     {
+        _galaxies = new(1024);
         _input = ParseInput().ToList();
         (_xGaps, _yGaps) = ExtractUniverseGaps();
     }
 
-    public override ValueTask<string> Solve_1()
+    public override ValueTask<string> Solve_1() => new($"{CalculateDistance(2)}");
+
+    public override ValueTask<string> Solve_2() => new($"{CalculateDistance(1_000_000)}");
+
+    private long CalculateDistance(int expansionFactor)
     {
-        var expandedInput = ExpandUniverse([.. _input.ConvertAll(l => l.ToList())]);
-
-        // Print(_input);
-
-        var galaxies = ExtractGalaxies(expandedInput);
-
-        int result = 0;
-        foreach (var subset in galaxies.Subsets(2))
-        {
-            result += (int)subset[0].ManhattanDistance(subset[1]);
-        }
-
-        return new($"{result}");
-    }
-
-    public override ValueTask<string> Solve_2()
-    {
-        const int expansionFactor = 1_000_000;
-
-        // Print(expandedInput);
-
-        var galaxies = ExtractGalaxies(_input);
-
         long result = 0;
-        foreach (var subset in galaxies.Subsets(2))
+        foreach (var subset in _galaxies.Subsets(2))
         {
-            result +=(long)subset[0].ManhattanDistance(subset[1]);
+            result += (long)subset[0].ManhattanDistance(subset[1]);
 
             var minX = Math.Min(subset[0].X, subset[1].X);
             var maxX = Math.Max(subset[0].X, subset[1].X);
-            for (int x = minX + 1; x < maxX; ++x)
+            foreach (var x in _xGaps)
             {
-                if (_xGaps.Contains(x))
+                if (x > minX && x < maxX)
                 {
                     result += expansionFactor - 1;
                 }
@@ -67,74 +50,19 @@ public class Day_11 : BaseDay
 
             var minY = Math.Min(subset[0].Y, subset[1].Y);
             var maxY = Math.Max(subset[0].Y, subset[1].Y);
-            for (int y = minY + 1; y < maxY; ++y)
+            foreach (var y in _yGaps)
             {
-                if (_yGaps.Contains(y))
+                if (y > minY && y < maxY)
                 {
                     result += expansionFactor - 1;
                 }
             }
         }
 
-        return new($"{result}");
+        return result;
     }
 
-    private List<List<Point>> ExpandUniverse(List<List<Point>> universe)
-    {
-        for (int y = 0; y < _yGaps.Count; ++y)
-        {
-            var inputIndex = _yGaps[y] + y;
-            universe.Insert(inputIndex, [.. universe[inputIndex]]);
-        }
-
-        var emptyPoint = new Point('.', -1, -1);
-
-        for (int x = 0; x < _xGaps.Count; ++x)
-        {
-            for (int y = 0; y < universe.Count; ++y)
-            {
-                universe[y].Insert(_xGaps[x] + x, emptyPoint);
-            }
-        }
-
-        return universe;
-    }
-
-    private List<List<Point>> ExpandUniverse_Part2(List<List<Point>> universe)
-    {
-        const int expansionFactor = 99;
-
-        // Expand on y
-        for (int y = 0; y < _yGaps.Count; ++y)
-        {
-            for (int i = 0; i < expansionFactor; ++i)
-            {
-                var inputIndex = _yGaps[y] + y * expansionFactor;
-                universe.Insert(inputIndex, [.. universe[inputIndex]]);
-            }
-        }
-
-        var emptyPoint = new Point('.', -1, -1);
-
-        Print(universe);
-
-        // Expand on x
-        for (int x = 0; x < _xGaps.Count; ++x)
-        {
-            for (int y = 0; y < universe.Count; ++y)
-            {
-                for (int i = 0; i < expansionFactor; ++i)
-                {
-                    int inputIndex = _xGaps[x] + x * expansionFactor;
-                    universe[y].Insert(inputIndex, emptyPoint);
-                }
-            }
-        }
-
-        return universe;
-    }
-
-    private  (List<int> xGaps, List<int> yGaps) ExtractUniverseGaps()
+    private (List<int> xGaps, List<int> yGaps) ExtractUniverseGaps()
     {
         List<int> yGaps = new(_input.Count);
         for (int y = 0; y < _input.Count; ++y)
@@ -177,24 +105,6 @@ public class Day_11 : BaseDay
         return (xGaps, yGaps);
     }
 
-    private static List<Point> ExtractGalaxies(List<List<Point>> universe)
-    {
-        var galaxies = new List<Point>(universe.Count / 10);
-
-        for (int y = 0; y < universe.Count; ++y)
-        {
-            for (int x = 0; x < universe[0].Count; ++x)
-            {
-                if (universe[y][x].IsGalaxy)
-                {
-                    galaxies.Add(new('#', x, y));
-                }
-            }
-        }
-
-        return galaxies;
-    }
-
     private IEnumerable<List<Point>> ParseInput()
     {
         var file = new ParsedFile(InputFilePath);
@@ -208,23 +118,17 @@ public class Day_11 : BaseDay
             int x = 0;
             foreach (var ch in line.ToList<char>())
             {
-                returnLine.Add(new(ch, x++, y));
+                Point point = new(ch, x++, y);
+                returnLine.Add(point);
+
+                if (point.IsGalaxy)
+                {
+                    _galaxies.Add(point);
+                }
             }
 
             y++;
             yield return returnLine;
-        }
-    }
-
-    private static void Print<T>(IEnumerable<IEnumerable<IntPointWithValue<T>>> grid)
-    {
-        for (int y = 0; y < grid.Count(); ++y)
-        {
-            for (int x = 0; x < grid.ElementAt(y).Count(); ++x)
-            {
-                Console.Write(grid.ElementAt(y).ElementAt(x).Value);
-            }
-            Console.WriteLine();
         }
     }
 }
---
+++ src/AoC_2023/Helpers.cs
@@ -0,0 +1,17 @@
+﻿using SheepTools.Model;
+
+namespace AoC_2023;
+internal static class Helpers
+{
+    public static void PrintGrid<T>(this IEnumerable<IEnumerable<IntPointWithValue<T>>> grid)
+    {
+        for (int y = 0; y < grid.Count(); ++y)
+        {
+            for (int x = 0; x < grid.ElementAt(y).Count(); ++x)
+            {
+                Console.Write(grid.ElementAt(y).ElementAt(x).Value);
+            }
+            Console.WriteLine();
+        }
+    }
+}
